8ac921423c6c8bb35d47d4cfb19e29730d85e40a
Adam Hoover <ashoover@gmail.com>
2018-04-03 00:44:40 -0500

    Nightly Push
    App Icon
    Window Title
    Encode Menu

--- decoder_ring.py
+++ decoder_ring.py
@@ -2,10 +2,11 @@ import datetime
 import random
 from tkinter import *
 import tkinter as tk
+import os
 
 
-screen_height = '600'
-screen_width = '800'
+screen_height = '200'
+screen_width = '300'
 
 profit_centers = {
     'A': 'Administration',
@@ -37,6 +38,7 @@ cuid = {
 }
 
 
+
 def random_number():
     return random.randrange(1000, 9999)
 
@@ -62,58 +64,90 @@ def decoder_ring(current_code):
     list_year = "201" + (code_list[3])
     list_uid = "".join(code_list[4:])
 
-    dash = '\n' + '----------------------------------' + '\n'
-
-    print(dash)
-    print('Current Code   : {}'.format(current_code))
-    print('CUID of client : {}'.format(full_cuid))
-    print('Profit Center  : {}'.format(full_profit_center))
-    print('Project Year   : {}'.format(list_year))
-    print('Project Number : {}'.format(list_uid))
-    print(dash)
-
     decode_dict['cuid'] = full_cuid
     decode_dict['fullpc'] = full_profit_center
     decode_dict['yr'] = list_year
     decode_dict['pn'] = list_uid
 
-    print(decode_dict)
     return decode_dict
+
+
+def encoder_ring():
+    encode = "Encode!!"
+    print(encode)
 
 
 ran_num = random_number()
 y_code = year_code()
 
 
-# GUI Setups
+# GUI
 
 def main_app():
     root = Tk()
 
+    # Encode
     ccode = decoder_ring(current_code='G3O88675')
     clientid = 'Client ID      : ' + ccode['cuid']
     pcenter = 'Profit Center  : ' + ccode['fullpc']
     year = 'Project Year   : ' + ccode['yr']
     pnum = 'Project Number : ' + ccode['pn']
 
-    time_button = tk.Button(root, text="Decode", command=ccode)
+    encode_button = tk.Button(root, text="Decode", command=ccode)
+    root.wm_title("Decoder Ring!")
     canvas = Canvas(root, width=screen_width, height=screen_height)
     label1 = Label(root, text=clientid)
     label2 = Label(root, text=pcenter)
     label3 = Label(root, text=year)
     label4 = Label(root, text=pnum)
 
-    time_button.pack()
+    encode_button.pack()
     label1.pack()
     label2.pack()
     label3.pack()
     label4.pack()
     canvas.pack()
 
+    # Decode
+
+    decode_v = StringVar()
+    decode_v.set('CU*BASE Operations')  # initialize radio buttons
+
+    pf_options = OptionMenu(root, decode_v, 'Administration',
+                                            'Accounting / Bookkeeping',
+                                            'Conversion / Deconversion',
+                                            'Disaster Recovery / Business Continuity',
+                                            'EFT / Money Movement',
+                                            'Forms Development',
+                                            'Graphics Design',
+                                            'Knowledge Development (Internal)',
+                                            'Lending / Collections',
+                                            'Marketing',
+                                            'Network Services / IT',
+                                            'CU*BASE Operations',
+                                            'Custom Program Development',
+                                            'Call Center (In / Out)',
+                                            'Sales',
+                                            'Teller / MSR',
+                                            'Video Production',
+                                            'Website Development',
+                                            'Bugs / AnswerBook Incidents',
+                                            'Client Education',
+                                            'Compliance / DORbusters')
+    pf_options.pack()
+
+    def ok():
+        print("value is", decode_v.get())
+        root.quit()
+
+    encode_button = Button(root, text="ENCODE!", command=ok())
+    encode_button.pack()
+
+    # Application Icon
+    root.iconphoto(True, PhotoImage(file=os.path.join("assets", "green_lantern_icon.png")))
     root.mainloop()
 
 
 # Temp Area
-decoder_ring('G3O88675')
 main_app()
 
